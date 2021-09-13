9231393ad2294c21aee98afa5b7e3cfe58efe1cd
jardel <jardel.urban@ibridge.com.br>
2021-09-13 17:41:22 -0300

    Criando todas as rotas, controllers e views necessarias para o site de maneira estatica

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Site\HomeController;
+use App\Http\Controllers\Site\CategoryController;
+use App\Http\Controllers\Site\BlogController;
+use App\Http\Controllers\Site\ContactController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::group(['namespace' => 'Site'], function () {
+    Route::get('/', [HomeController::class, '__invoke']);
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::get('produtos', [CategoryController::class, 'index']);
+    Route::get('produtos/{slug}', [CategoryController::class, 'show']);
+
+    Route::get('blog', [BlogController::class, '__invoke']);
+
+    Route::view('sobre', view:'site.about.index');
+
+    Route::get('contato', [ContactController::class, 'index']);
+    Route::post('contato', [ContactController::class, 'form']);
 });
+
